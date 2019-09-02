e178c82de5546f1fdc9475de4504de433773565a
yoyomasky <yoyomasky@gmail.com>
2019-09-02 17:30:13 +0800

    modify

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -68,7 +68,10 @@ class UsersController extends Controller
      */
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+        return view('users.show', compact('user', 'statuses'));
     }
     /**
      * 创建用户->方法
--- app/Models/User.php
+++ app/Models/User.php
@@ -49,11 +49,16 @@ class User extends Authenticatable
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return $this->gravatarLink . $hash . '?s=' . $size;
     }
-    public static function boot(){
+    public static function boot()
+    {
         parent::boot();
 
         static::creating(function ($user) {
             $user->activation_token = Str::random(10);
         });
+    }
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
     }
 }
