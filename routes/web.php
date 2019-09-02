93368bec7d2028396fd3af132f7ec42254cd7686
yoyomasky <yoyomasky@gmail.com>
2019-09-02 13:59:44 +0800

    modify

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,23 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
-Route::get('signup','UsersController@create')->name('signup');
+Route::get('signup', 'UsersController@create')->name('signup');
 
 Route::resource('users', 'UsersController');
 
-Route::get('login','SessionsController@create')->name('login');
-Route::post('login','SessionsController@store')->name('login');
-Route::delete('login','SessionsController@destroy')->name('logout');
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('login', 'SessionsController@destroy')->name('logout');
 
+/**
+ * 验证激活邮箱
+ */
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+/**
+ * 密码重置相关
+ */
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
