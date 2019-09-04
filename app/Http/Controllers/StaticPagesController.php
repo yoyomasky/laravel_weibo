045ec10d0bba23a6f4443151ab930d7b4241cc88
yoyomasky <yoyomasky@gmail.com>
2019-09-04 13:28:01 +0800

    modify

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,20 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StaticPagesController extends Controller
 {
     //
-    public function home(){
-        return view('static_pages.home');
+    public function home()
+    {
+        $feed_items = [];
+        if (Auth::check()) {
+            $feed_items = Auth::user()->feed()->paginate(5);
+        }
+        return view('static_pages.home', compact('feed_items'));
     }
 
-    public function help(){
+    public function help()
+    {
         return view('static_pages.help');
     }
 
-    public function about(){
+    public function about()
+    {
         return view('static_pages.about');
     }
-
 }
