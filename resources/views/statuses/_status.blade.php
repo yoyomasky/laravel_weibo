bfc1aeb7ee8556bca4479933c6c084b0f3944c0c
yoyomasky <yoyomasky@gmail.com>
2019-09-04 14:07:52 +0800

    modify

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -6,4 +6,12 @@
     <h5 class="mt-0 mb-1">{{ $user->name }} <small> / {{ $status->created_at->diffForHumans() }}</small></h5>
     {{ $status->content }}
   </div>
+
+  @can('destroy',$status)
+    <form action="{{ route('statuses.destroy', $status->id) }}" method="POST" onsubmit="return confirm('您確定要刪除本條微博嗎?');">
+      {{ csrf_field() }}
+      {{ method_field('DELETE') }}
+      <button type="submit" class="btn btn-sm btn-danger">删除</button>
+    </form>
+  @endcan
 </li>
