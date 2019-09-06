ecea5a2df2ac0a02fa446b4f25be1e8d74960067
yoyomasky <yoyomasky@gmail.com>
2019-09-06 18:55:55 +0800

    modify

---
+++ app/Http/Controllers/FollowersController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class FollowersController extends Controller
+{
+    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(User $user)
+    {
+        $this->authorize('follow', $user);
+
+        if (!Auth::user()->isFollowing($user->id)) {
+            Auth::user()->follow($user->id);
+        }
+
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('follow', $user);
+
+        if (Auth::user()->isFollowing($user->id)) {
+            Auth::user()->unfollow($user->id);
+        }
+
+        return redirect()->route('users.show', $user->id);
+    }
+}
