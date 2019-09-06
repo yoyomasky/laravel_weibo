6a142697613e2b13ccee8f8f9d4b910f52d0ffc1
yoyomasky <yoyomasky@gmail.com>
2019-09-06 18:55:31 +0800

    modify

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -26,4 +26,7 @@ class UserPolicy
     {
         return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
+    public function follow(User $currentUser,User $user){
+        return $currentUser->id !== $user->id;
+    }
 }
